2c07462f940596c9d520150b74fa1b295634cec8
Waqas7616 <waqasali761611@gmail.com>
2024-02-04 15:07:26 +0500

    installation complete

---
+++ crypto-news/src/app/coins/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Coins() {
+    return (
+        <div>
+            <h1>My coins</h1>
+        </div>
+    )
+}
+
+export default Coins
---
+++ crypto-news/src/app/page.js
@@ -0,0 +1,7 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <a href="/coins" className="text-[lime] text-4xl font-bold">waqas ali</a>
+  );
+}
